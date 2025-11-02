da9fd700dd7c55d878ae31f2034363c6990d1759
yoselyng123 <m.yoselyn@correo.unimet.edu.ve>
2025-11-02 01:00:30 -0400

    Inicio del proyecto

---
+++ src/components/Carta.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Carta() {
+  return <div>Carta</div>;
+}
+
+export default Carta;
---
+++ src/screens/VistaMazo.jsx
@@ -0,0 +1,11 @@
+import Carta from '../components/Carta';
+
+function VistaMazo() {
+  return (
+    <div>
+      <Carta />
+    </div>
+  );
+}
+
+export default VistaMazo;
